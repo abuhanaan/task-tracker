be67039844f59dfc382d4a794db926d86fa6609a
Mustopha Qomorudeen <adewunmi99@gmail.com>
2022-12-24 16:08:46 +0100

    bug-fix

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/Task';
-import { TaskService } from 'src/app/servies/task.service';
+import { TaskService } from 'src/app/services/task.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
